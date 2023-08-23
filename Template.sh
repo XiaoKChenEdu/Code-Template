1318bb705e3ae02b3061727d4f91617fa0919955
Xiaokang Chen <Xiaokang.chen888@gmail.com>
2023-08-22 17:29:31 -1000

    Added Template for c c++ sh py

---
+++ Template.sh
@@ -0,0 +1,11 @@
+#!/bin/{shell}
+###############################################################################
+#######        University of Hawaii, College of Engineering
+####### @brief {assignmentname} - {projectname} - {class} - {semster} {yyyy}
+#######
+####### @file {filename}.c
+####### @version 1.0
+#######
+####### @author Xiaokang Chen <xiaokang@hawaii.edu>
+####### @date dd mmm yyyy
+###############################################################################
